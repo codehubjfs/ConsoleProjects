32808f31c72ffa23c182412cb1f3cefeef6746d9
Piratheesh26 <92738701+Piratheesh26@users.noreply.github.com>
2024-07-30 19:22:28 +0530

    Add files via upload

---
+++ Piratheesh/Task Management/Oracle/taskmanagement.sql
@@ -0,0 +1,78 @@
+INSERT INTO employee (EMP_ID, NAME, EMAIL, PASSWORD, GENDER, PHONE_NUMB, ROLE, CITY, MAG_ID, HIREDATE) VALUES (1, 'MRKing', 'king123@gmail.com', 'King123$', 'M', '9999988888', 'president', 'Delhi', 1, TO_DATE('14-JAN-18', 'DD-MON-YY'));
+INSERT INTO employee (EMP_ID, NAME, EMAIL, PASSWORD, GENDER, PHONE_NUMB, ROLE, CITY, MAG_ID, HIREDATE) VALUES (2, 'Abiilash', 'abi123@gmail.com', 'Abiilash@123', 'M', '9768457965', 'employee', 'Coimbatore', 3, TO_DATE('12-JAN-20', 'DD-MON-YY'));
+INSERT INTO employee (EMP_ID, NAME, EMAIL, PASSWORD, GENDER, PHONE_NUMB, ROLE, CITY, MAG_ID, HIREDATE) VALUES (3, 'Deenesh', 'Deenesh123@gmail.com', 'Deenesh1$', 'M', '7845986754', 'manager', 'Chennai', 1, TO_DATE('14-JUN-20', 'DD-MON-YY'));
+INSERT INTO employee (EMP_ID, NAME, EMAIL, PASSWORD, GENDER, PHONE_NUMB, ROLE, CITY, MAG_ID, HIREDATE) VALUES (5, 'Rajesh', 'rajesh123@gmail.com', 'Rajesh123%', 'M', '8978789654', 'employee', 'Bengaluru', 3, TO_DATE('22-JUL-20', 'DD-MON-YY'));
+INSERT INTO employee (EMP_ID, NAME, EMAIL, PASSWORD, GENDER, PHONE_NUMB, ROLE, CITY, MAG_ID, HIREDATE) VALUES (6, 'Janathanan', 'Jana123@gmail.com', 'Jana123$', 'M', '9778997754', 'manager', 'bengaluru', 1, TO_DATE('22-FEB-19', 'DD-MON-YY'));
+INSERT INTO employee (EMP_ID, NAME, EMAIL, PASSWORD, GENDER, PHONE_NUMB, ROLE, CITY, MAG_ID, HIREDATE) VALUES (7, 'Ramesh', 'ramesh123@gmail.com', 'Ramesh123$', 'M', '6380727272', 'employee', 'Coimbatore', 6, TO_DATE('19-OCT-21', 'DD-MON-YY'));
+INSERT INTO employee (EMP_ID, NAME, EMAIL, PASSWORD, GENDER, PHONE_NUMB, ROLE, CITY, MAG_ID, HIREDATE) VALUES (8, 'Ramkumar', 'ram123@gmail.com', 'Ram123$$', 'M', '7766985475', 'employee', 'Coimbatore', 6, TO_DATE('26-AUG-21', 'DD-MON-YY'))
+INSERT INTO employee (EMP_ID, NAME, EMAIL, PASSWORD, GENDER, PHONE_NUMB, ROLE, CITY, MAG_ID, HIREDATE) VALUES (9, 'Raji', 'raji123@gmail.com', 'welcome@123', 'F', '6379845275', 'employee', 'Coimbatore', 6, TO_DATE('26-JAN-20', 'DD-MON-YY'));
+INSERT INTO employee (EMP_ID, NAME, EMAIL, PASSWORD, GENDER, PHONE_NUMB, ROLE, CITY, MAG_ID, HIREDATE) VALUES (21, 'sanjai', 'sanjai1234@gmail.com', 'Sanjai123@', 'M', '9977857755', 'employee', 'Tutucorin', 24, TO_DATE('28-JUN-18', 'DD-MON-YY'));
+INSERT INTO employee (EMP_ID, NAME, EMAIL, PASSWORD, GENDER, PHONE_NUMB, ROLE, CITY, MAG_ID, HIREDATE) VALUES (22, 'sweathaa', 'swetha812@gmail.com', 'Swetha812$', 'F', '9768579684', 'employee', 'Coimbatore', 3, TO_DATE('25-JAN-22', 'DD-MON-YY'));
+INSERT INTO employee (EMP_ID, NAME, EMAIL, PASSWORD, GENDER, PHONE_NUMB, ROLE, CITY, MAG_ID, HIREDATE) VALUES (23, 'Madhavkumar', 'madhav123@gmail.com', 'Madhav123$', 'M', '6385742968', 'employee', 'Chennai', 6, TO_DATE('25-JUN-23', 'DD-MON-YY'));
+INSERT INTO employee (EMP_ID, NAME, EMAIL, PASSWORD, GENDER, PHONE_NUMB, ROLE, CITY, MAG_ID, HIREDATE) VALUES (24, 'Vijaykumar', 'vijaykumar@gmail.com', 'Vijay123$', 'M', '7655203410', 'manager', 'Chennai', 1, TO_DATE('12-OCT-23', 'DD-MON-YY'));
+INSERT INTO employee (EMP_ID, NAME, EMAIL, PASSWORD, GENDER, PHONE_NUMB, ROLE, CITY, MAG_ID, HIREDATE) VALUES (41, 'Vignesh123', 'vignesh123@gmail.com', 'Vignesh123$', 'M', '9764310252', 'employee', 'Karaikudi', 24, TO_DATE('12-MAY-20', 'DD-MON-YY'));
+
+INSERT INTO employee (EMP_ID, NAME, EMAIL, PASSWORD, GENDER, PHONE_NUMB, ROLE, CITY, MAG_ID, HIREDATE) VALUES (62, 'Gowthman12', 'gowthaman@gmail.com', 'Gowthaman1$', 'M', '798546574', 'manager', 'Coimbatore', 1, TO_DATE('15-FEB-14', 'DD-MON-YY'));
+INSERT INTO employee (EMP_ID, NAME, EMAIL, PASSWORD, GENDER, PHONE_NUMB, ROLE, CITY, MAG_ID, HIREDATE) VALUES (63, 'Gowthman12', 'gowthman@gmail.com', 'Gowthaman1$', 'M', '798585964', 'manager', 'coimbatore', 1, TO_DATE('12-JUN-14', 'DD-MON-YY'));
+
+INSERT INTO transcation (EMP_ID, TASK_ID, STATUS_ID, ASSIGNER_ID) VALUES (6, 3, 2, NULL);
+INSERT INTO transcation (EMP_ID, TASK_ID, STATUS_ID, ASSIGNER_ID) VALUES (7, 3, 3, NULL);
+INSERT INTO transcation (EMP_ID, TASK_ID, STATUS_ID, ASSIGNER_ID) VALUES (21, 3, 3, NULL);
+INSERT INTO transcation (EMP_ID, TASK_ID, STATUS_ID, ASSIGNER_ID) VALUES (2, 3, 3, 3);
+INSERT INTO transcation (EMP_ID, TASK_ID, STATUS_ID, ASSIGNER_ID) VALUES (8, 3, 2, NULL);
+INSERT INTO transcation (EMP_ID, TASK_ID, STATUS_ID, ASSIGNER_ID) VALUES (21, 4, 5, NULL);
+INSERT INTO transcation (EMP_ID, TASK_ID, STATUS_ID, ASSIGNER_ID) VALUES (2, 4, 4, 3);
+INSERT INTO transcation (EMP_ID, TASK_ID, STATUS_ID, ASSIGNER_ID) VALUES (9, 4, 4, NULL);
+INSERT INTO transcation (EMP_ID, TASK_ID, STATUS_ID, ASSIGNER_ID) VALUES (7, 4, 2, NULL);
+INSERT INTO transcation (EMP_ID, TASK_ID, STATUS_ID, ASSIGNER_ID) VALUES (3, 5, 3, NULL);
+INSERT INTO transcation (EMP_ID, TASK_ID, STATUS_ID, ASSIGNER_ID) VALUES (3, 6, 4, NULL);
+INSERT INTO transcation (EMP_ID, TASK_ID, STATUS_ID, ASSIGNER_ID) VALUES (3, 9, 5, NULL);
+INSERT INTO transcation (EMP_ID, TASK_ID, STATUS_ID, ASSIGNER_ID) VALUES (3, 10, 1, NULL);
+INSERT INTO transcation (EMP_ID, TASK_ID, STATUS_ID, ASSIGNER_ID) VALUES (5, 10, 1, 3);
+INSERT INTO transcation (EMP_ID, TASK_ID, STATUS_ID, ASSIGNER_ID) VALUES (2, 23, 4, 3);
+INSERT INTO transcation (EMP_ID, TASK_ID, STATUS_ID, ASSIGNER_ID) VALUES (21, 23, 2, NULL);
+INSERT INTO transcation (EMP_ID, TASK_ID, STATUS_ID, ASSIGNER_ID) VALUES (2, 24, 5, 3);
+INSERT INTO transcation (EMP_ID, TASK_ID, STATUS_ID, ASSIGNER_ID) VALUES (5, 62, 1, 3);
+INSERT INTO transcation (EMP_ID, TASK_ID, STATUS_ID, ASSIGNER_ID) VALUES (2, 63, 3, 3);
+INSERT INTO transcation (EMP_ID, TASK_ID, STATUS_ID, ASSIGNER_ID) VALUES (5, 66, 1, 3);
+INSERT INTO transcation (EMP_ID, TASK_ID, STATUS_ID, ASSIGNER_ID) VALUES (2, 66, 4, 3);
+INSERT INTO transcation (EMP_ID, TASK_ID, STATUS_ID, ASSIGNER_ID) VALUES (2, 66, 4, 3);
+
+INSERT INTO admin (ADMIN_ID, NAME, ADMIN_PASSWORD, EMAIL) VALUES (1, 'aakash', 'aakash123', 'aakash123@gmail.com');
+INSERT INTO admin (ADMIN_ID, NAME, ADMIN_PASSWORD, EMAIL) VALUES (22, 'Piratheesh', 'Prathish1$', 'piratheesh@gmail.com');
+INSERT INTO admin (ADMIN_ID, NAME, ADMIN_PASSWORD, EMAIL) VALUES (2, 'Jana', 'Jana123$', 'jana123@gmail.com');
+
+INSERT INTO task (TASK_ID, TASK_NAME, TASK_DESP, START_DAT, END_DATE, TASK_PRIORTIY) VALUES (2, 'Task Management', 'Develop an tak management application in which it will be accessed by 3 different employee', TO_DATE('20-JUN-24', 'DD-MON-YY'), TO_DATE('29-JUL-24', 'DD-MON-YY'), 'Low');
+INSERT INTO task (TASK_ID, TASK_NAME, TASK_DESP, START_DAT, END_DATE, TASK_PRIORTIY) VALUES (3, 'BackEnd', 'Wanted TO create a backend connectivity with oracle sql', TO_DATE('10-JAN-22', 'DD-MON-YY'), TO_DATE('29-JAN-22', 'DD-MON-YY'), 'Low');
+INSERT INTO task (TASK_ID, TASK_NAME, TASK_DESP, START_DAT, END_DATE, TASK_PRIORTIY) VALUES (4, 'BackEnd 1', 'Create a backend connectivity to task management system', TO_DATE('13-MAY-24', 'DD-MON-YY'), TO_DATE('25-MAY-24', 'DD-MON-YY'), 'Low');
+INSERT INTO task (TASK_ID, TASK_NAME, TASK_DESP, START_DAT, END_DATE, TASK_PRIORTIY) VALUES (5, 'Hotel Management', 'Develop an Hotel Management application in which it will be accessed by 4 different employee', TO_DATE('16-MAY-24', 'DD-MON-YY'), TO_DATE('02-JUL-24', 'DD-MON-YY'), 'high');
+INSERT INTO task (TASK_ID, TASK_NAME, TASK_DESP, START_DAT, END_DATE, TASK_PRIORTIY) VALUES (6, 'Leave Management', 'Develop an Leave Management application in which it will be accessed by 3 different employee', TO_DATE('01-APR-24', 'DD-MON-YY'), TO_DATE('01-MAY-24', 'DD-MON-YY'), 'high');
+INSERT INTO task (TASK_ID, TASK_NAME, TASK_DESP, START_DAT, END_DATE, TASK_PRIORTIY) VALUES (9, 'Bank Management', 'Develop an Bank Management application in which it will have different operation for different users', TO_DATE('02-JUN-24', 'DD-MON-YY'), TO_DATE('29-JUN-24', 'DD-MON-YY'), 'high');
+INSERT INTO task (TASK_ID, TASK_NAME, TASK_DESP, START_DAT, END_DATE, TASK_PRIORTIY) VALUES (10, 'American Express', 'Develop an Master card application in which it will have different operation for different users', TO_DATE('29-JUN-24', 'DD-MON-YY'), TO_DATE('10-AUG-24', 'DD-MON-YY'), 'high');
+INSERT INTO task (TASK_ID, TASK_NAME, TASK_DESP, START_DAT, END_DATE, TASK_PRIORTIY) VALUES (23, 'UI UX Design', 'Wanted to create UIUX design for helathcare application', TO_DATE('29-MAY-24', 'DD-MON-YY'), TO_DATE('12-JUN-24', 'DD-MON-YY'), 'high'); 
+INSERT INTO task (TASK_ID, TASK_NAME, TASK_DESP, START_DAT, END_DATE, TASK_PRIORTIY) VALUES (24, 'database Connect', 'Want to connect with database', TO_DATE('12-JUN-24', 'DD-MON-YY'), TO_DATE('20-JUN-24', 'DD-MON-YY'), 'low');
+INSERT INTO task (TASK_ID, TASK_NAME, TASK_DESP, START_DAT, END_DATE, TASK_PRIORTIY) VALUES (42, 'Assign Task', 'Task should be assigned to every team members', TO_DATE('04-JUL-24', 'DD-MON-YY'), TO_DATE('08-JUL-24', 'DD-MON-YY'), 'medium');
+INSERT INTO task (TASK_ID, TASK_NAME, TASK_DESP, START_DAT, END_DATE, TASK_PRIORTIY) VALUES (62, 'Mybatis', 'Create an application with mybatis framework', TO_DATE('10-JUL-24', 'DD-MON-YY'), TO_DATE('21-JUL-24', 'DD-MON-YY'), 'high');
+INSERT INTO task (TASK_ID, TASK_NAME, TASK_DESP, START_DAT, END_DATE, TASK_PRIORTIY) VALUES (63, 'FrontEnd', 'Do every front verification  for all users', TO_DATE('07-JUL-24', 'DD-MON-YY'), TO_DATE('16-JUL-24', 'DD-MON-YY'), 'high');
+INSERT INTO task (TASK_ID, TASK_NAME, TASK_DESP, START_DAT, END_DATE, TASK_PRIORTIY) VALUES (65, 'qwertyjkllkjhgfd', 'asdfghjkl', TO_DATE('18-JUL-24', 'DD-MON-YY'), TO_DATE('09-JUL-24', 'DD-MON-YY'), 'medium');
+INSERT INTO task (TASK_ID, TASK_NAME, TASK_DESP, START_DAT, END_DATE, TASK_PRIORTIY) VALUES (66, 'Validation', 'Do every front verification  for all users and validation', TO_DATE('17-JUL-24', 'DD-MON-YY'), TO_DATE('27-JUL-24', 'DD-MON-YY'), 'high');
+INSERT INTO task (TASK_ID, TASK_NAME, TASK_DESP, START_DAT, END_DATE, TASK_PRIORTIY) VALUES (82, 'Task Creations', 'dtf ghjkl hgyvt', TO_DATE('18-JUL-24', 'DD-MON-YY'), TO_DATE('02-JUL-24', 'DD-MON-YY'), NULL);
+INSERT INTO task (TASK_ID, TASK_NAME, TASK_DESP, START_DAT, END_DATE, TASK_PRIORTIY) VALUES (83, 'Review Task 1', 'Review Task 1', TO_DATE('01-JUL-24', 'DD-MON-YY'), TO_DATE('15-JUL-24', 'DD-MON-YY'), 'high');
+
+INSERT INTO personal_task (TASK_ID, TASK_NAME, TASK_DESP, START_DAT, END_DATE, TASK_PRIORITY, EMAIL, STATUS) VALUES (1, 'FrontEnd', 'Wanted to Create an Frontend Page', TO_DATE('28-JUN-24', 'DD-MON-YY'), TO_DATE('05-JUL-24', 'DD-MON-YY'), 'Low', 'abi123@gmail.com', 'Assigned');
+INSERT INTO personal_task (TASK_ID, TASK_NAME, TASK_DESP, START_DAT, END_DATE, TASK_PRIORITY, EMAIL, STATUS) VALUES (4, 'BackEnd', 'Wanted to Perform and Interactive', TO_DATE('27-JUN-24', 'DD-MON-YY'), TO_DATE('14-JUL-24', 'DD-MON-YY'), 'High', 'abi123@gmail.com', 'Assigned'); 
+INSERT INTO personal_task (TASK_ID, TASK_NAME, TASK_DESP, START_DAT, END_DATE, TASK_PRIORITY, EMAIL, STATUS) VALUES (6, 'Clear Search Bar', 'Wanted To clear every no needed search bar', TO_DATE('27-JUN-24', 'DD-MON-YY'), TO_DATE('27-JUN-24', 'DD-MON-YY'), 'high', 'abi123@gmail.com', 'Assigned');
+INSERT INTO personal_task (TASK_ID, TASK_NAME, TASK_DESP, START_DAT, END_DATE, TASK_PRIORITY, EMAIL, STATUS) VALUES (8, 'Task Assign', 'Task Should be Assigned To team Members', TO_DATE('03-JUL-24', 'DD-MON-YY'), TO_DATE('14-JUL-24', 'DD-MON-YY'), 'Low', 'Deenesh123@gmail.com', 'Assigned');
+INSERT INTO personal_task (TASK_ID, TASK_NAME, TASK_DESP, START_DAT, END_DATE, TASK_PRIORITY, EMAIL, STATUS) VALUES (9, 'Task Assign', 'Task Should be Assigned To team Members', TO_DATE('04-JUL-24', 'DD-MON-YY'), TO_DATE('06-JUL-24', 'DD-MON-YY'), 'Low', NULL, 'Assigned');
+INSERT INTO personal_task (TASK_ID, TASK_NAME, TASK_DESP, START_DAT, END_DATE, TASK_PRIORITY, EMAIL, STATUS) VALUES (10, 'Task Assign', 'Task Should be Assigned To team Members', TO_DATE('04-JUL-24', 'DD-MON-YY'), TO_DATE('06-JUL-24', 'DD-MON-YY'), 'Low', NULL, 'Assigned');
+INSERT INTO personal_task (TASK_ID, TASK_NAME, TASK_DESP, START_DAT, END_DATE, TASK_PRIORITY, EMAIL, STATUS) VALUES (11, 'Mybatis', 'Wanted to do mybatis application', TO_DATE('09-JUL-24', 'DD-MON-YY'), TO_DATE('11-JUL-24', 'DD-MON-YY'), 'high', 'abi123@gmail.com', 'Assigned'); 
+INSERT INTO personal_task (TASK_ID, TASK_NAME, TASK_DESP, START_DAT, END_DATE, TASK_PRIORITY, EMAIL, STATUS) VALUES (21, 'Mybatis', 'Wanted to do mybatis application', TO_DATE('11-JUL-24', 'DD-MON-YY'), TO_DATE('12-JUL-24', 'DD-MON-YY'), 'high', 'Deenesh123@gmail.com', 'Assigned');
+INSERT INTO personal_task (TASK_ID, TASK_NAME, TASK_DESP, START_DAT, END_DATE, TASK_PRIORITY, EMAIL, STATUS) VALUES (22, 'HealthCare App', 'HealthCare application webpage should have an interactive design', TO_DATE('10-JUL-24', 'DD-MON-YY'), TO_DATE('28-JUL-24', 'DD-MON-YY'), 'Medium', 'abi123@gmail.com', 'Assigned');
+INSERT INTO personal_task (TASK_ID, TASK_NAME, TASK_DESP, START_DAT, END_DATE, TASK_PRIORITY, EMAIL, STATUS) VALUES (23, 'Assessment Model', 'Create an page to assign Question to the student', TO_DATE('11-JUL-24', 'DD-MON-YY'), TO_DATE('27-JUL-24', 'DD-MON-YY'), 'high', 'Deenesh123@gmail.com', 'Assigned');
+INSERT INTO personal_task (TASK_ID, TASK_NAME, TASK_DESP, START_DAT, END_DATE, TASK_PRIORITY, EMAIL, STATUS) VALUES (25, 'HealthCare App', 'HealthCare application webpage should have an interactive design', TO_DATE('11-JUL-24', 'DD-MON-YY'), TO_DATE('19-JUL-24', 'DD-MON-YY'), 'Low', 'Deenesh123@gmail.com', 'Assigned');
+
+INSERT INTO task_status (ID, STATUS) VALUES (1, 'Assigned');
+INSERT INTO task_status (ID, STATUS) VALUES (2, 'Started');
+INSERT INTO task_status (ID, STATUS) VALUES (3, 'Pending');
+INSERT INTO task_status (ID, STATUS) VALUES (4, 'Completed'); 
+INSERT INTO task_status (ID, STATUS) VALUES (5, 'Overlayed');
